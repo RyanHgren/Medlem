425c159a505e54d661295805017383bbb9ee7a6f
RyanHgren <ryanhellgren@gmail.com>
2025-10-29 23:45:43 +0100

    la till primary key och namn till medlemmar

---
+++ medlem/src/main/java/se/RyanHgren/medlem/MedlemType.java
@@ -0,0 +1,41 @@
+package se.RyanHgren.medlem;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class MedlemType {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String namn;
+
+    public MedlemType() {
+    }
+
+    public MedlemType(String namn) {
+        this.namn = namn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }
+    
+}
