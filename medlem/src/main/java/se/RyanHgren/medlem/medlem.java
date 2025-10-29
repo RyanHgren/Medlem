0c24583bfe3b34483ea5cb7464940ebe98ff663a
RyanHgren <ryanhellgren@gmail.com>
2025-10-29 12:21:12 +0100

    Första commit - firstPush

---
+++ medlem/src/main/java/se/RyanHgren/medlem/medlem.java
@@ -0,0 +1,11 @@
+package se.RyanHgren.medlem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medlem {
+    public static void main(String[] args) {
+        SpringApplication.run(Medlem.class, args);
+    }
+}
