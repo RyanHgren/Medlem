fc310a9e906cb4b19775388d6caf90bf8fe90ee5
RyanHgren <ryanhellgren@gmail.com>
2025-10-30 20:40:31 +0100

    raderade lite kod som inte används

--- src/main/java/se/ryan/medlem/MedlemType.java
+++ src/main/java/se/ryan/medlem/MedlemType.java
@@ -1,16 +1,7 @@
 package se.ryan.medlem;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 
-@Entity
 public class MedlemType {
-
-
-    @Id
-    @GeneratedValue
-    private Long id;
     private String namn;
 
     public MedlemType() {
@@ -18,14 +9,6 @@ public class MedlemType {
 
     public MedlemType(String namn) {
         this.namn = namn;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getNamn() {
